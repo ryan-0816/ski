f8f7d26123b3192cc07ce8e89387396a5c77e5df
ryantagen <rst4035@g.rit.edu>
2025-08-11 11:28:25 -0400

    handle pretender

--- svelte.config.js
+++ svelte.config.js
@@ -3,15 +3,17 @@ import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
-	// Consult https://svelte.dev/docs/kit/integrations
-	// for more information about preprocessors
 	preprocess: vitePreprocess(),
 
 	kit: {
-		// adapter-auto only supports some environments, see https://svelte.dev/docs/kit/adapter-auto for a list.
-		// If your environment is not supported, or you settled on a specific environment, switch out the adapter.
-		// See https://svelte.dev/docs/kit/adapters for more information about adapters.
-		adapter: adapter()
+		adapter: adapter(),
+
+		prerender: {
+			handleHttpError: ({ status, path }) => {
+				console.warn(`Prerender error ${status} at ${path}`);
+				return 'skip'; // Skip this path but continue build
+			}
+		}
 	}
 };
 
