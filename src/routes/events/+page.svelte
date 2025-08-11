24891e331e7ab4c57c02f93239626b76e4b73d2b
ryantagen <rst4035@g.rit.edu>
2025-08-11 11:29:13 -0400

    calendar fix

--- src/routes/events/+page.svelte
+++ src/routes/events/+page.svelte
@@ -5,5 +5,5 @@
 
 <div class="text-column">
 	<h1>Events</h1>
-    <iframe src="calendar link (https://calendar.google.com/calendar/embed?src=xs3171%40g.rit.edu&ctz=America%2FNew_York)" style="border: 0" width="800" height="600" frameborder="0" scrolling="no"></iframe>
+    <iframe src="https://calendar.google.com/calendar/embed?src=xs3171%40g.rit.edu&ctz=America%2FNew_York" style="border: 0" width="800" height="600" frameborder="0" scrolling="no"></iframe>
 </div>
